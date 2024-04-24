f977bf6610e8d23d167555f7af7cc3b5f7e54e05
Gaby Rodrigues <127845196+gabzinh4@users.noreply.github.com>
2024-04-24 22:45:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const botoes = document.querySelectorAll('.botao');
+const textos = document.querySelectorAll('.aba-conteudo');
+for(let i=0;i < botoes.length; i++){
+  botoes[i].onclick = function() {
+    for(let k=0; k < botoes.length; k++) {
+      botoes[k].classList.remove('ativo');
+       textos[k].classList.remove('ativo');
+      textos[i].classList.add('ativo');
+    }
+  botoes[i].classList.add('ativo');
+    textos[i].classList.add('ativo');
+  }
+}
+
+//iniciar os contadores 
+const contadores = document.querySelectorAll('.contador'); 
+
+let dataHoje = new Date();
+
+contadores[0].textContent(dataHoje);
